1254887cb42abdaf28f2df00515003fb78e322ca
David Dan <davidwdan@gmail.com>
2017-07-14 12:14:22 -0700

    Fixed test for php 5.6 support

--- test/WebSocketClientTransportProviderTest.php
+++ test/WebSocketClientTransportProviderTest.php
@@ -17,18 +17,21 @@ class WebSocketClientTransportProviderTest extends TestCase
     /**
      * @expectedException \InvalidArgumentException
      */
-    public function testConstructorNonWebSocketAddress() {
+    public function testConstructorNonWebSocketAddress()
+    {
         $transportProvider = new WebSocketClientTransportProvider('http://127.0.0.1/ws');
     }
 
     /**
      * @expectedException \InvalidArgumentException
      */
-    public function testConstructorInvalidUri() {
+    public function testConstructorInvalidUri()
+    {
         $transportProvider = new WebSocketClientTransportProvider('wss://:/');
     }
 
-    public function testWssWillRequestTlsWithDefaultPort() {
+    public function testWssWillRequestTlsWithDefaultPort()
+    {
         $connector = $this->getMockBuilder(ConnectorInterface::class)
             ->setMethods(['connect'])
             ->getMock();
@@ -36,17 +39,19 @@ class WebSocketClientTransportProviderTest extends TestCase
         $connector->expects($this->once())
             ->method('connect')
             ->with('tls://127.0.0.1:443')
-            ->willReturn(new Promise(function () {}));
+            ->willReturn(new Promise(function () {
+            }));
 
         $transportProvider = new WebSocketClientTransportProvider('wss://127.0.0.1/', $connector);
 
         $client = $this->createMock(Client::class);
-        $loop = $this->createMock(LoopInterface::class);
+        $loop   = $this->createMock(LoopInterface::class);
 
         $transportProvider->startTransportProvider($client, $loop);
     }
 
-    public function testWsWillRequestWithDefaultPort() {
+    public function testWsWillRequestWithDefaultPort()
+    {
         $connector = $this->getMockBuilder(ConnectorInterface::class)
             ->setMethods(['connect'])
             ->getMock();
@@ -54,17 +59,19 @@ class WebSocketClientTransportProviderTest extends TestCase
         $connector->expects($this->once())
             ->method('connect')
             ->with('127.0.0.1:80')
-            ->willReturn(new Promise(function () {}));
+            ->willReturn(new Promise(function () {
+            }));
 
         $transportProvider = new WebSocketClientTransportProvider('ws://127.0.0.1/', $connector);
 
         $client = $this->createMock(Client::class);
-        $loop = $this->createMock(LoopInterface::class);
+        $loop   = $this->createMock(LoopInterface::class);
 
         $transportProvider->startTransportProvider($client, $loop);
     }
 
-    public function testWssWillRequestWithSpecificPort() {
+    public function testWssWillRequestWithSpecificPort()
+    {
         $connector = $this->getMockBuilder(ConnectorInterface::class)
             ->setMethods(['connect'])
             ->getMock();
@@ -72,17 +79,19 @@ class WebSocketClientTransportProviderTest extends TestCase
         $connector->expects($this->once())
             ->method('connect')
             ->with('tls://127.0.0.1:444')
-            ->willReturn(new Promise(function () {}));
+            ->willReturn(new Promise(function () {
+            }));
 
         $transportProvider = new WebSocketClientTransportProvider('wss://127.0.0.1:444/', $connector);
 
         $client = $this->createMock(Client::class);
-        $loop = $this->createMock(LoopInterface::class);
+        $loop   = $this->createMock(LoopInterface::class);
 
         $transportProvider->startTransportProvider($client, $loop);
     }
 
-    public function testWsWillRequestWithSpecificPort() {
+    public function testWsWillRequestWithSpecificPort()
+    {
         $connector = $this->getMockBuilder(ConnectorInterface::class)
             ->setMethods(['connect'])
             ->getMock();
@@ -90,39 +99,43 @@ class WebSocketClientTransportProviderTest extends TestCase
         $connector->expects($this->once())
             ->method('connect')
             ->with('127.0.0.1:81')
-            ->willReturn(new Promise(function () {}));
+            ->willReturn(new Promise(function () {
+            }));
 
         $transportProvider = new WebSocketClientTransportProvider('ws://127.0.0.1:81/', $connector);
 
         $client = $this->createMock(Client::class);
-        $loop = $this->createMock(LoopInterface::class);
+        $loop   = $this->createMock(LoopInterface::class);
 
         $transportProvider->startTransportProvider($client, $loop);
     }
 
-    public function testCloseAfterSendingRequest() {
-        $ds = new ThroughStream();
-        $connector = new class($ds) implements ConnectorInterface {
-            private $ds;
-
-            public function __construct(DuplexStreamInterface $ds)
-            {
-                $this->ds = $ds;
-            }
-
-            public function connect($uri)
-            {
-                return new FulfilledPromise(new ConnectionStub($this->ds));
-            }
-        };
+    public function testCloseAfterSendingRequest()
+    {
+        $ds        = new ThroughStream();
+        $connector = new TestConnector($ds);
 
         $client = $this->createMock(Client::class);
-        $loop = $this->createMock(LoopInterface::class);
+        $loop   = $this->createMock(LoopInterface::class);
 
         $transportProvider = new WebSocketClientTransportProvider('ws://127.0.0.1/', $connector);
 
         $transportProvider->startTransportProvider($client, $loop);
+    }
+}
 
 
+class TestConnector implements ConnectorInterface
+{
+    private $ds;
+
+    public function __construct(DuplexStreamInterface $ds)
+    {
+        $this->ds = $ds;
+    }
+
+    public function connect($uri)
+    {
+        return new FulfilledPromise(new ConnectionStub($this->ds));
     }
 }
